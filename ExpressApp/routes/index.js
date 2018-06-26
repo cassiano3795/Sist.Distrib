90470eb92a1c705f2d8d27afedc0c6935c2013c4
Cassiano Raupp <cassiano.raupp@zeeng.com.br>
2018-06-26 11:37:30 -0300

    Commit inicial.

--- ExpressApp/routes/index.js
+++ ExpressApp/routes/index.js
@@ -7,4 +7,26 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+/* GET users listing. */
+router.get('/getAvaibleYears', function (req, res) {
+    //TODO: CHAMAR FUNCTION QUE VAI NO MEMCACHED, BUSCA OS SERVIDORES ATIVOS, E RETORNA OS ANOS
+    res.send('respond with a resource');
+});
+
+router.get('/getData/:year', function (req, res) {
+    res.send('respond with a Data');
+});
+
+function getAvaibleYears() {
+
+}
+
+function getDataMySql() { //TODO: PARAMETROS DE BUSCA MYSQL
+
+}
+
+function getDataMemCached() { //TODO: PARAMETROS DE BUSCA MEMCACHED
+
+}
+
 module.exports = router;
